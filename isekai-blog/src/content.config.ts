438d01cf497fce71140b76c51ca1cdc6e2e133f6
myonsuke <faulheit033@gmail.com>
2026-05-09 03:14:13 +0900

    first commit

---
+++ isekai-blog/src/content.config.ts
@@ -0,0 +1,24 @@
+import { defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
+import { z } from 'astro/zod';
+
+const blog = defineCollection({
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			subtitle1: z.string().optional(),
+            subtitle2: z.string().optional(),
+			description: z.string(),
+			// Transform string to Date object
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			heroImage: z.optional(image()),
+			
+			youtubeTrack: z.string().optional(),
+		}),
+});
+
+export const collections = { blog };
