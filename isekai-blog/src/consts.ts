4f9daf0425ff5a9801361ecb002e1efb4f31767f
myonsuke <faulheit033@gmail.com>
2026-05-13 10:39:41 +0900

    rename

--- isekai-blog/src/consts.ts
+++ isekai-blog/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '書斎、あるいは秘密基地';
-export const SITE_DESCRIPTION = '失われやすいものを、失わぬように。';
+export const SITE_TITLE = '三好ハイツ１０２';
+export const SITE_DESCRIPTION = 'モラトリアムの象徴、あるいはセミの抜け殻';
